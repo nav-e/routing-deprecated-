24fa108dc5b535e2ab3a688b7be5178aa8ac2e53
Pavel Danilov <pdanilov94@gmail.com>
2017-06-26 16:58:08 +0300

    Get rid of redundant constructors

--- src/main/java/io/greennav/routing/RoadGraphSpecifics.java
+++ src/main/java/io/greennav/routing/RoadGraphSpecifics.java
@@ -1,26 +1,12 @@
 package io.greennav.routing;
 
 import de.topobyte.osm4j.core.model.impl.Node;
-import org.jgrapht.graph.EdgeSetFactory;
-import org.jgrapht.graph.specifics.DirectedEdgeContainer;
 import org.jgrapht.graph.specifics.FastLookupDirectedSpecifics;
-import java.util.Map;
 import java.util.Set;
 
 public class RoadGraphSpecifics extends FastLookupDirectedSpecifics<Node, RoadEdge> {
     RoadGraphSpecifics(RoadGraph roadGraph) {
         super(roadGraph);
-    }
-
-    RoadGraphSpecifics(RoadGraph roadGraph,
-                       Map<Node, DirectedEdgeContainer<Node, RoadEdge>> vertexMap) {
-        super(roadGraph, vertexMap);
-    }
-
-    RoadGraphSpecifics(RoadGraph roadGraph,
-                       Map<Node, DirectedEdgeContainer<Node, RoadEdge>> vertexMap,
-                       EdgeSetFactory<Node, RoadEdge> edgeEdgeSetFactory) {
-        super(roadGraph, vertexMap, edgeEdgeSetFactory);
     }
 
     private RoadGraph getRoadGraph() {
